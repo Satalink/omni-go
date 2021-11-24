f48d330eb53f5afa0a1cb0b0be25e0a5a73529e8
Neal Garrett <neal.garrett@gmail.com>
2021-11-23 20:57:47 -0500

    update initialization

--- src/constant.js
+++ src/constant.js
@@ -3,21 +3,105 @@ export const defaultGroups = [
     name: "Default",
     id: "default",
     prefix: false
+  },
+  {
+    name: "Google",
+    id: "google",
+    prefix: "g"
   }
 ];
 
 export const defaultShortcuts = [
   {
-    keyword: "app",
-    url:
-      "https://chrome.google.com/webstore/detail/keyword-redirect/okmndadncjgigahlgcbgenaoecagebhh/",
+    keyword: "fb",
+    url: "https://facebook.com",
     group: "default"
   },
   {
-    keyword: "dev",
-    url: "https://twitter.com/sasi513",
+    keyword: "twit",
+    url: "https://twitter.com",
     group: "default"
-  }
+  },
+  {
+    keyword: "ttok",
+    url: "https://tictok.com",
+    group: "default"
+  },
+  {
+    keyword: "yt",
+    url: "https://youtube.com",
+    group: "default"
+  },
+  {
+    keyword: "ama",
+    url: "https://amazon.com",
+    group: "default"
+  },
+  {
+    keyword: "gplay",
+    url: "https://play.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gmail",
+    url: "https://gmail.com/",
+    group: "google"
+  },
+  {
+    keyword: "gcal",
+    url: "https://calendar.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gduo",
+    url: "https://duo.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gmeet",
+    url: "https://meet.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gchat",
+    url: "https://chat.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gvoice",
+    url: "https://voice.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gdocs",
+    url: "https://docs.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gkeep",
+    url: "https://keep.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gmaps",
+    url: "https://maps.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gdrive",
+    url: "https://drive.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gearth",
+    url: "https://earth.google.com/",
+    group: "google"
+  },
+  {
+    keyword: "gpod",
+    url: "https://podcasts.google.com/",
+    group: "google"
+  },
 ];
 
 export const encodeHTML = function (str) {
