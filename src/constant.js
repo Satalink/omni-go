78e8c5b0e70c05f712160ad4110056dd5e7d3150
Neal Garrett <neal.garrett@gmail.com>
2025-08-18 06:29:25 -0400

    removed unused permissions and broken defaults links

--- src/constant.js
+++ src/constant.js
@@ -10,6 +10,31 @@ export const defaultGroups = [
     prefix: false
   },
   {
+    name: "Social",
+    id: "social",
+    prefix: false,
+  },
+  {
+    name: "Crypto",
+    id: "crypto",
+    prefix: false,
+  },
+  {
+    name: "AI",
+    id: "ai",
+    prefix: false,
+  },
+  {
+    name: "Media",
+    id: "media",
+    prefix: false,
+  },
+  {
+    name: "Shopping",
+    id: "shopping",
+    prefix: false,
+  },
+  {
     name: "News",
     id: "news",
     prefix: false
@@ -33,32 +58,99 @@ export const defaultShortcuts = [
     keyname: "Facebook",
     keyword: "fb",
     url: "https://facebook.com/",
-    group: "Default"
+    group: "Social"
   },
   {
-    keyname: "Twitter",
-    keyword: "twit",
-    url: "https://twitter.com/",
-    group: "Default"
+    keyname: "X",
+    keyword: "x",
+    url: "https://x.com/",
+    group: "Social"
   },
   {
-    keyname: "TikTok",
-    keyword: "tik",
-    url: "https://tiktok.com/",
-    group: "Default"
+    keyname: "Reddit",
+    keyword: "red",
+    url: "https://reddit.com/",
+    group: "Social"
   },
   {
     keyname: "YouTube",
     keyword: "yt",
     url: "https://youtube.com/",
-    group: "Default"
+    group: "Media"
+  },
+  {
+    keyname: "Rumble",
+    keyword: "rum",
+    url: "https://rumble.com",
+    group: "Media"
   },
   {
     keyname: "Amazon",
     keyword: "amz",
     url: "https://amazon.com/",
-    group: "Default"
+    group: "Shopping"
   },
+  {
+    keyname: "Walmart",
+    keyword: "wm",
+    url: "https://walmart.com",
+    group: "Shopping"
+  },
+  {
+    keyname: "Kraken",
+    keyword: "kra",
+    url: "https://kraken.com",
+    group: "Crypto"
+  },
+  {
+    keyname: "DexScreener",
+    keyword: "dex",
+    url: "https://dexscreener.com",
+    group: "Crypto"
+  },  
+  {
+    keyname: "GekoTerminal",
+    keyword: "geko",
+    url: "https://gekoterminal.com",
+    group: "Crypto"
+  },  
+  {
+    keyname: "SolScan",
+    keyword: "sol",
+    url: "https://solscan.io",
+    group: "Crypto"
+  },  
+  {
+    keyname: "Gemini",
+    keyword: "gem",
+    url: "https://gemini.google.com",
+    group: "AI"
+  },
+  {
+    keyname: "Grok",
+    keyword: "grok",
+    url: "https://grok.com",
+    group: "AI"
+  },
+  {
+    keyname: "ChatGPT",
+    keyword: "chat",
+    url: "https://ghatgpt.com",
+    group: "AI"
+  },
+  {
+    keyname: "CoPilot",
+    keyword: "cp",
+    url: "https://copilot.microsoft.com",
+    group: "AI"
+  },
+  {
+    keyname: "Gemini",
+    keyword: "g",
+    url: "https://gemini.google.com",
+    group: "AI"
+  },
+
   {
     keyname: "Google Play",
     keyword: "play",
@@ -102,10 +194,16 @@ export const defaultShortcuts = [
     group: "Google"
   },
   {
-    keyname: "Google Podcasts",
+    keyname: "Google Music",
     keyword: "pods",
-    url: "https://podcasts.google.com/",
+    url: "https://music.google.com/",
     group: "Google"
+  },
+    {
+    keyname: "INOreader",
+    keyword: "news",
+    url: "https://inoreader.com",
+    group: "News"
   },
   {
     keyname: "Weather",
